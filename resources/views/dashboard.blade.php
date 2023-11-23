0dde273977941c906e7ec54206f5f06f877bbc77
Haki <57068224+GhiffariHaki@users.noreply.github.com>
2023-11-23 23:58:20 +0700

    Navbar Fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,17 +2,15 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<section class="vh-100 overflow-hidden" style="background-image: url({{asset('img/Background.jpg')}}); background-color: #cccccc;">
+    <div class="align-items-center text-white flex-column my-auto">
+        <h1> Kreanesia </h1>
+    </div>
+</section>
 
-                <div class="card-body">
-                    <h1> YO NIBBA WELCOME TO DASHBOARD</h2>
-                </div>
-            </div>
-        </div>
+<div class="container-fluid" style="background-color: #FFFF00;">
+    <div class="vh-100 overflow-hidden">
+        <h1> </h1>
     </div>
 </div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,44 +22,42 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css'>
 
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
         @stack('head')
     </head>
 
     <body>
-        <div id="app">
-            <nav class="navbar navbar-expand-md navbar-dark bg-success shadow-sm">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/home') }}">
-                        <img src="{{ asset('img/LogoKreanesia.png')}}" width="120" height="40" class="d-inline-block align-bottom" alt="">
+        <div>
+            <nav class="navbar navbar-expand-lg shadow sticky-top" style="background-color: #FFFFFF;">
+                <div class="container-fluid">
+                    <a class="navbar-brand px-4" href="/">
+                        <img src="{{ asset('img/KreanesiaOnlyDark.png')}}" width="120" height="40" class="d-inline-block align-bottom" alt="">
                     </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
-
-                        </ul>
-
                         <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
+                        <ul class="nav nav-pills ms-auto mb-2 mb-lg-0 px-4">
                             <li class="nav-item">
-                                <a class="nav-link" href="/"><span class="fas fa-home"></span> Home</a>
+                                <a class="nav-link {{ (request()->is('/')) ? 'active' : '' }}" href="/"><span class="fas fa-home"></span> Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/kupon"><span class="fas fa-leaf"></span> Kupon</a>
+                                <a class="nav-link {{ (request()->is('kupon')) ? 'active' : '' }}" href="/kupon"><span class="fas fa-receipt"></span> Kupon</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/about"><span class="fas fa-receipt"></span> About</a>
+                                <a class="nav-link {{ (request()->is('about')) ? 'active' : '' }}" href="/about"><span class="fas fa-users"></span> About</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
 
-            <main class="py-4">
+            <main>
                 @yield('content')
             </main>
         </div>
