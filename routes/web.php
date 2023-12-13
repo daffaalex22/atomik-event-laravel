62d4eda4c7bf0651ec8e2cf252543bc600e5624b
Daffa' Alexander <daffaalex22@gmail.com>
2023-12-13 21:55:12 +0700

    SnapToken Obtained

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::get('/', function () {
 });
 
 Route::get('/kupon', function () {
+
+    $snapToken = session()->get('snapToken');
+
     return view('kupon');
 });
 
@@ -31,6 +34,12 @@ Route::post('/kupon', function () {
         "email"=> request("email"),
         "coupon"=> request("coupon"),
     ];
+
+    // Split Into First and Last Name
+    $string = $data["name"];
+    $last_space = strrpos($string, " ");
+    $data["first_name"] = substr($string, 0, $last_space);
+    $data["last_name"] = substr($string, $last_space + 1);
 
 
     // Getting Counter Data
@@ -56,7 +65,32 @@ Route::post('/kupon', function () {
     
     $fp = fopen($counter_file, "w"); 
     fwrite($fp, $counter);
-    fclose($fp); 
+    fclose($fp);
 
-    return redirect('/kupon');
+    // Midtrans Payment
+
+    // Set your Merchant Server Key
+    \Midtrans\Config::$serverKey = 'SB-Mid-server-03XnJAZR--SJtcDSjace9Mpk';
+    // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+    \Midtrans\Config::$isProduction = false;
+    // Set sanitization on (default)
+    \Midtrans\Config::$isSanitized = true;
+
+    $params = array(
+        'transaction_details' => array(
+            'order_id' => "{$data["startCount"]}",
+            'gross_amount' => $data["coupon"],
+        ),
+        'customer_details' => array(
+            'first_name' => $data["first_name"],
+            'last_name' => $data["last_name"],
+            'email' => $data["email"]
+        ),
+    );
+
+    $snapToken = \Midtrans\Snap::getSnapToken($params);
+
+    return redirect('/kupon')->with([
+        'snapToken'=> $snapToken
+    ]);
 });
